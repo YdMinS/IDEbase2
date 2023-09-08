32289db90c82fcb5c19fb59001aeb12cdf868ab3
ydmins <dr.ydmins@gmail.com>
2023-09-08 14:44:12 +0900

    first init

---
+++ src/main/java/com/ydmins/IDEbase2/IdEbase2Application.java
@@ -0,0 +1,13 @@
+package com.ydmins.IDEbase2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdEbase2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdEbase2Application.class, args);
+	}
+
+}
